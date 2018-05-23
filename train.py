6f3a0a90fe31a727b233be52e74f83fa1b7d8294
Calle Gunnarsson <calle.gunnarsson@gmail.com>
2018-05-23 21:58:34 +0200

    Fix for python3 and tensorflow 1.x and swedish plates

--- train.py
+++ train.py
@@ -36,8 +36,10 @@ import functools
 import glob
 import itertools
 import multiprocessing
+import random
 import sys
 import time
+import os
 
 import cv2
 import numpy
@@ -62,13 +64,13 @@ def code_to_vec(p, code):
 def read_data(img_glob):
     for fname in sorted(glob.glob(img_glob)):
         im = cv2.imread(fname)[:, :, 0].astype(numpy.float32) / 255.
-        code = fname.split("/")[1][9:15]
-        p = fname.split("/")[1][17] == '1'
+        code = fname.split(os.sep)[1][9:15]
+        p = fname.split(os.sep)[1][16] == '1'
         yield im, code_to_vec(p, code)
 
 
 def unzip(b):
-    xs, ys = zip(*b)
+    xs, ys = list(zip(*b))
     xs = numpy.array(xs)
     ys = numpy.array(ys)
     return xs, ys
@@ -95,7 +97,7 @@ def mpgen(f):
 
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
-        q = multiprocessing.Queue(1)
+        q = multiprocessing.Queue(3)
         proc = multiprocessing.Process(target=main,
                                        args=(q, args, kwargs))
         proc.start()
@@ -110,7 +112,7 @@ def mpgen(f):
     return wrapped
 
 
-@mpgen
+#@mpgen
 def read_batches(batch_size):
     g = gen.generate_ims()
     def gen_vecs():
@@ -124,19 +126,18 @@ def read_batches(batch_size):
 def get_loss(y, y_):
     # Calculate the loss from digits being incorrect.  Don't count loss from
     # digits that are in non-present plates.
-    digits_loss = tf.nn.softmax_cross_entropy_with_logits(
-                                          tf.reshape(y[:, 1:],
+    digits_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+                                          logits=tf.reshape(y[:, 1:],
                                                      [-1, len(common.CHARS)]),
-                                          tf.reshape(y_[:, 1:],
+                                          labels=tf.reshape(y_[:, 1:],
                                                      [-1, len(common.CHARS)]))
-    digits_loss = tf.reshape(digits_loss, [-1, common.PLATE_LEN]) # 7
+    digits_loss = tf.reshape(digits_loss, [-1, common.PLATE_LEN])
     digits_loss = tf.reduce_sum(digits_loss, 1)
     digits_loss *= (y_[:, 0] != 0)
     digits_loss = tf.reduce_sum(digits_loss)
 
     # Calculate the loss from presence indicator being wrong.
-    presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(
-                                                          y[:, :1], y_[:, :1])
+    presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
     presence_loss = common.PLATE_LEN * tf.reduce_sum(presence_loss)
 
     return digits_loss, presence_loss, digits_loss + presence_loss
@@ -180,7 +181,7 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
         assert len(params) == len(initial_weights)
         assign_ops = [w.assign(v) for w, v in zip(params, initial_weights)]
 
-    init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
 
     def vec_to_plate(v):
         return "".join(common.CHARS[i] for i in v)
@@ -227,7 +228,7 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
         if initial_weights is not None:
             sess.run(assign_ops)
 
-        test_xs, test_ys = unzip(list(read_data("test/*.png"))[:50])
+        test_xs, test_ys = unzip(list(read_data(os.path.join("test","*.png")))[:50])
 
         try:
             last_batch_idx = 0
@@ -260,6 +261,6 @@ if __name__ == "__main__":
 
     train(learn_rate=0.001,
           report_steps=20,
-          batch_size=10,
+          batch_size=50,
           initial_weights=initial_weights)
 
